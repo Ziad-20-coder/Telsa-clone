e829eab03278df6821176c7f1259e65c223c199d
Ziad <ziad@gmail.com>
2026-04-14 23:52:52 +0200

    good looking desktop navbar

--- src/Components/Navigations/DesktopNavigation/DesktopNavContent.tsx
+++ src/Components/Navigations/DesktopNavigation/DesktopNavContent.tsx
@@ -6,13 +6,15 @@ interface DesktopNavContentProps {
 
 const DesktopNavContent = ({ data }: DesktopNavContentProps) => {
   return (
-    <div className="absolute top-full left-0">
-      {data.map((item) => (
-        <li key={item.id}>
-          <h3>{item.title}</h3>
-          <img src={item.image} alt={item.title} />
-        </li>
-      ))}
+    <div className="bg-white shadow-md absolute top-full left-0 min-w-full">
+      <div className="grid grid-cols-3 gap-5 max-w-2/3 mx-auto mb-6">
+        {data.map((item) => (
+          <li key={item.id}>
+            <img src={item.image} alt={item.title} className="h-46 m-auto" />
+            <h3 className="text-center">{item.title}</h3>
+          </li>
+        ))}
+      </div>
     </div>
   )
 }
