5cc9172fc7dd191234b5590eaa85ceb53a6a6b84
Ziad <ziadosamacoder@gmail.com>
2026-04-24 03:07:32 +0300

    added a generic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Hero from "./Components/Hero"
 import Navigation from "./Components/Navigation"
+import Card from "./Components/UI/Carousel/Card"
 import Carousel from "./Components/UI/Carousel/Carousel"
+import type { SnapCarousel } from "./Types/services"
 
 function App() {
 
@@ -8,7 +10,7 @@ function App() {
     <>
       <Navigation />
       <Hero />
-      <Carousel url={'/src/data/Carousel1.json'}/>
+      <Carousel<SnapCarousel> renderItems={() => <Card />} url={'/src/data/Carousel1.json'}/>
     </>
   )
 }
--- src/Components/UI/Carousel/Carousel.tsx
+++ src/Components/UI/Carousel/Carousel.tsx
@@ -1,19 +1,28 @@
 import { useFetch } from "../../../Hooks/useFetch"
 
-interface CarouselProps {
+interface CarouselProps<T> {
   url: string
+  renderItems: (item: T) => React.ReactNode
 }
 
-const Carousel = ({url}: CarouselProps) => {
+interface dynamicType<T> {
+  carousel: T[]
+}
 
-  const {data, isLoading, error} = useFetch(url)
+const Carousel = <Type,>({url, renderItems}: CarouselProps<Type>) => {
+
+  const {data, isLoading, error} = useFetch<dynamicType<Type>>(url)
 
   if(isLoading) return <h1>Loading...</h1>
   else if(error || !data) return <h1>Failed to fetch data.</h1>
 
   return (
     <div>
-
+      {
+        data?.carousel.map((card) => (
+          renderItems(card)
+        ))
+      }
     </div>
   )
 }
